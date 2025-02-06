cc6fa01a384fbe070e47cb483bc7aa29f2ee3ebe
Yuchan <kaiwow@naver.com>
2025-02-06 17:59:38 +0900

    'fixed'

---
+++ SWEA/4834_SWEA.py
@@ -0,0 +1,14 @@
+# T = int(input())
+# for test_case in range(1, T + 1):
+#
+#     N = int(input())
+#     arr = list(map(int, input()))
+#
+#
+#     many_num = 0
+#
+#     for i in range(N):
+#
+#
+#
+#     print(f'#{test_case} {max(arr)} {arr.count(max(arr))}')
